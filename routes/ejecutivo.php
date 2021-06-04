9f892eb0e98b4bbcf8df4950766ccef4ba2498d6
Victor Arana <victor@codersfree.com>
2021-06-04 00:10:42 -0500

    actualizacion

--- routes/ejecutivo.php
+++ routes/ejecutivo.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Livewire\Ejecutivo\Solicitudes;
 use App\Http\Controllers\Ejecutivo\SolicitudController;
-use App\Http\Livewire\SolicitudesAsignadas;
 
 Route::get('/', Solicitudes::class)->name('ejecutivo.index');
-Route::get('solicitudes/asignadas', SolicitudesAsignadas::class)->name('ejecutivo.solicitudes.asignadas');
 Route::get('solicitudes/{solicitud}', [SolicitudController::class, 'show'])->name('ejecutivo.solicitudes.show');
