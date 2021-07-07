9d14e0c79d65fbef1112a245887f12070467b5cb
Victor Arana <victor@codersfree.com>
2021-07-07 14:24:27 -0500

    Envio de correo

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -7,6 +7,9 @@ use App\Models\Solicitud;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SolicitudCreate;
+
 class SolicitudController extends Controller
 {
 
@@ -21,7 +24,9 @@ class SolicitudController extends Controller
     public function update(Solicitud $solicitud){
         $solicitud->status = 2;
         $solicitud->save();
+
         $correo = auth()->user()->email;
+        Mail::to($correo)->send(new SolicitudCreate());
 
         return redirect('/solicitudes')->with('info', 'Su solicitud a sido enviada a Revisión, un ejecutivo se contactara con Usted. Se ha enviado un correo como respaldo a la siguiente dirección '.$correo);
     }
