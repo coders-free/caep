c6662c56f48a8a98a00a486ea53b0ced6547fdaf
cgsaavedrac <claudiosaavedra@pesic.cl>
2021-07-07 15:09:44 -0400

    Cambios Menores 3

--- routes/cargas.php
+++ routes/cargas.php
@@ -11,14 +11,15 @@ use Illuminate\Support\Facades\Storage;
 
 Route::get('carga/imponentes', function () {
 
-    $fecha_actual = now()->format('Ymd');
+    //$fecha_actual = now()->format('Ymd');
+    $fecha_actual = "20210610";
 
     $file_imponente = 'Acceso FTP/Proveedores/ma/Repositorios/' . $fecha_actual . "_MACKENNA_CARGARIMPONENTES.csv";
     $file_credito = 'Acceso FTP/Proveedores/ma/Repositorios/' . $fecha_actual .'_MACKENNA_ESTADOCUENTA.csv';
     
-    if (Storage::disk('ftp')->exists($file_imponente)) {
-        Excel::import(new CargaImponenteImport, $file_imponente, 'ftp');
-    }
+    //if (Storage::disk('ftp')->exists($file_imponente)) {
+    //    Excel::import(new CargaImponenteImport, $file_imponente, 'ftp');
+    //}
 
     if (Storage::disk('ftp')->exists($file_credito)) {
         Excel::import(new CargaCreditoImport, $file_credito, 'ftp');
